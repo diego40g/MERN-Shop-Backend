2a35240dc9f89c9a063783cedb69d57398d23d87
diego40g <diegopaznaula@hotmail.com>
2023-07-09 23:40:46 -0500

    adding all parameters of product

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,11 +1,59 @@
 import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
-    name: String,
-    image: String,
+    name: {
+        type: String,
+        require: true,
+    },
+    description: {
+        type: String,
+        require: true,
+    },
+    richDescription: {
+        type: String,
+        default: ''
+    },
+    image: {
+        type: String,
+        default: ''
+    },
+    images: [{
+        type: String
+    }],
+    brand: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        require: true
+    },
     countInStock: {
         type: Number,
-        required: true
+        required: true,
+        min: 0,
+        max: 255
+    },
+    rating: {
+        type: Number,
+        default: 0,
+    },
+    numReviews: {
+        type: Number,
+        default: 0
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
     }
 });
 
