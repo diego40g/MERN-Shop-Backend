3e1e08f85701809c52e1dedd505e4c119f4e063c
diego40g <diegopaznaula@hotmail.com>
2023-07-10 23:45:03 -0500

    adding delete http request

--- src/routers/categories.ts
+++ src/routers/categories.ts
@@ -12,4 +12,39 @@ categoriesRoute.get('/',async(req,res)=>{
     res.send(categoriesList)
 })
 
+categoriesRoute.post('/', async(req, res) => {
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color,
+        image: req.body.image
+    })
+    category=await category.save();
+    if(!category)
+        return res.status(404).send('The category cannot be created!!!')
+    
+    res.send(category);
+})
+
+categoriesRoute.delete('/:id', (req, res)=>{
+    Category.findByIdAndRemove(req.params.id).then(category=>{
+        if(category) {
+            return res.status(200).json({
+                success: true,
+                message: 'The category is deleted!!!'
+            })
+        }else{
+            return res.status(404).json({
+                success: false,
+                message: 'Category not found!!!'
+            })
+        }
+    }).catch(err => {
+        return res.status(400).json({
+            success: false,
+            error: err
+        })
+    })
+})
+
 export default categoriesRoute;
