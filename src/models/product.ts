f277f5b4d4c292907e1ed073fe6c25bf431b65a4
diego40g <diegopaznaula@hotmail.com>
2023-07-20 23:41:54 -0500

    creating a interface document and adding a virtual key of id

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,6 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema, Document, Model } from 'mongoose';
 
-const productSchema = new mongoose.Schema({
+interface IProduct extends Document {
+    name: string;
+    description: string;
+    richDescription: string;
+    image: string;
+    images: Array<string>;
+    brand: string;
+    price: number;
+    category: mongoose.Schema.Types.ObjectId;
+    countInStock: number;
+    rating: number;
+    numReviews: number;
+    isFeatured: boolean;
+    dateCreated: Date;
+}
+
+const productSchema: Schema<IProduct> = new mongoose.Schema({
     name: {
         type: String,
         require: true,
@@ -54,7 +70,15 @@ const productSchema = new mongoose.Schema({
     dateCreated: {
         type: Date,
         default: Date.now
-    }
+    },
 });
 
-export const Product = mongoose.model('Product', productSchema);
+productSchema.virtual('id').get(function(this: Document) {
+    return this._id.toHexString();
+})
+
+productSchema.set('toJSON', {
+    virtuals: true,
+})
+
+export const Product: Model<IProduct> = mongoose.model('Product', productSchema);
