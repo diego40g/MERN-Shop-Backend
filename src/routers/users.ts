879258731c3a6b67d4e7df3ce56582c897494b45
diego40g <diegopaznaula@hotmail.com>
2023-07-22 23:39:12 -0500

    adding get and post request get all and create one user

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -1,15 +1,36 @@
-import express from 'express';
-import { User } from "../models/user";
+import express, { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
+import userSchema, { User } from "../models/user";
 
 const usersRouter = express.Router();
 
-usersRouter.get(`/`, async (req, res) =>{
+usersRouter.get(`/`, async (req: Request, res: Response): Promise<void> => {
     const userList = await User.find();
 
     if(!userList) {
         res.status(500).json({success: false})
     } 
-    res.send(userList);
+    res.status(200).send(userList);
+});
+usersRouter.post(`/`, async (req: Request, res: Response) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        street: req.body.street,
+        passwordHash: bcrypt.hashSync(req.body.passwordHash, process.env.PASSWORD_HASH),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country
+    })
+    const addUser = await user.save();
+
+    if(!user)
+        return res.status(500).send('The user cannot be created!');
+    
+    res.status(201).json(addUser)
 })
 
 export default usersRouter;
