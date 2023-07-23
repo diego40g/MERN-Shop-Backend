1dc9ff7ac52ba12df042f6625b42b2504c2cf0ef
diego40g <diegopaznaula@hotmail.com>
2023-07-22 23:43:03 -0500

    updating params and creating a virtual id to user model shema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,12 +1,68 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema, Document, Model } from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
+interface IUser extends Document {
+    name: string;
+    email: string;
+    passwordHash: string;
+    street: string;
+    apartment: string;
+    city: string;
+    zip: string;
+    country: string;
+    phone: string;
+    isAdmin: boolean;
+}
+
+const userSchema: Schema<IUser> = new mongoose.Schema({
+    name: {
+        type: String,
         required: true
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    street: {
+        type: String,
+        default: ''
+    },
+    apartment: {
+        type: String,
+        default: ''
+    },
+    zip: {
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: ''
     }
 })
 
-export const User = mongoose.model('User', userSchema);
+userSchema.virtual('id').get(function(this: Document) {
+    return this._id.toHexString();
+})
+
+userSchema.set('toJSON', {
+    virtuals: true,
+})
+
+export const User: Model<IUser> = mongoose.model('User', userSchema);
+export default userSchema;
