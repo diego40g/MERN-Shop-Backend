41a5a12bde5070a6fdf7cd88d9f0435187ff3f39
diego40g <diegopaznaula@hotmail.com>
2023-07-22 23:40:45 -0500

    updating properties and new virtual id into the model of user

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import userSchema, { User } from "../models/user";
+import * as dotenv from 'dotenv';
 
+dotenv.config({path:'./.env'});
 const usersRouter = express.Router();
 
 usersRouter.get(`/`, async (req: Request, res: Response): Promise<void> => {
