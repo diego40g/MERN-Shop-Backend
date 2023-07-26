b5afbe287cf44e19265a52838209b84bd9bef5dd
diego40g <diegopaznaula@hotmail.com>
2023-07-26 00:05:12 -0500

    updating errorhandle funtion to validation error and server errors

--- src/helpers/error_handle.ts
+++ src/helpers/error_handle.ts
@@ -1,8 +1,12 @@
 import { Request, Response, NextFunction  } from 'express';
 
-export const errorhandle=(err: Error, req: Request, res: Response, next: NextFunction) => {
+export const errorHandle=(err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err.name === "UnauthorizedError") {
-        return res.status(401).json({ message: "Unauthorized" });
+        return res.status(401).json({ message: "The user is not authorized" });
+    }
+    if (err.name === "ValidationError") {
+        return res.status(401).json({ message: err });
     }
     next()
+    return res.status(500).json(err);
 }
