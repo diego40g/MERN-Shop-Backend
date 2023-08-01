6a70b1bdd82f20220e562ae7ce6090facff2d654
diego40g <diegopaznaula@hotmail.com>
2023-07-31 23:41:03 -0500

    some help to use isRevoked

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -1,14 +1,29 @@
-import { expressjwt } from "express-jwt";
+/*import { expressjwt } from "express-jwt";
 import * as dotenv from 'dotenv';
+import { JwtPayload } from "jsonwebtoken";
+import { Request } from 'express';
 
 dotenv.config({path:'./.env'});
 
 const secretKey = process.env.JWT_SECRET;
 const api = process.env.API_URL;
+const tokenBlacklist: Set<string> = new Set();
+type IsRevokedFunction = (req: Request, payload: JwtPayload, done: (err: Error | null, revoked: boolean, reason?: any) => void) => void;
+const customIsRevoked: IsRevokedFunction = (req, payload, done) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    const isRevoked = token ? tokenBlacklist.has(token) : false;
+  
+    done(null, isRevoked);
+};
+/*if(!payload.isAdmin){
+            done(null, true);
+        }
+        done();/
+
 const authJwt = expressjwt({ 
     secret: secretKey!, 
     algorithms: ['HS256'], 
-    isRevoked: revoked!,
+    isRevoked: customIsRevoked,
 }).unless({
     path: [
         {url: /\/api\/v1\/product(.*)/, methods: ['GET', 'OPTIONS']},
@@ -18,12 +33,43 @@ const authJwt = expressjwt({
     ]
 })
 
-const revoked = async(req: Request, payload: any, done: any) => {
-    if(!payload.isAdmin){
-        done(null, true);
-    }
 
-    done();
+
+export default authJwt;*/
+import expressJwt, { Request } from 'express-jwt';
+import { JwtPayload } from 'jsonwebtoken';
+
+const api = process.env.API_URL;
+
+// Tipo para la función isRevoked
+type IsRevokedFunction = (req: Request, payload: JwtPayload, done: (error: Error | null, revoked?: boolean) => void) => void;
+
+// Función isRevoked
+const isRevoked: IsRevokedFunction = (req, payload, done) => {
+  if (!payload.isAdmin) {
+    done(null, true);
+  } else {
+    done(null, false);
+  }
+};
+
+function authJwt() {
+  const secret = process.env.secret;
+  
+  return expressJwt({
+    secret,
+    algorithms: ['HS256'],
+    isRevoked: isRevoked
+  }).unless({
+    path: [
+      { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
+      { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+      { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
+      `${api}/users/login`,
+      `${api}/users/register`,
+    ]
+  });
 }
 
 export default authJwt;
