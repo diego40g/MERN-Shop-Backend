a6857dae65a35e6642a18133cf1b795d90a374a8
diego40g <diegopaznaula@hotmail.com>
2023-08-09 23:57:56 -0500

    adding a popualte and sort to order and show information

--- src/routers/orders.ts
+++ src/routers/orders.ts
@@ -4,6 +4,14 @@ import { OrderItem } from '../models/order-item';
 const ordersRouter = express.Router();
 
 ordersRouter.get(`/`, async (req, res) =>{
+    const orderList = await Order.find().populate('user','name').sort({'dateOrdered':-1});
+
+    if(!orderList) {
+        res.status(500).json({success: false})
+    } 
+    res.send(orderList);
+})
+ordersRouter.get(`/all`, async (req, res) =>{
     const orderList = await Order.find();
 
     if(!orderList) {
