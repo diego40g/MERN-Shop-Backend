11e0ba7188ca5b0cfd0d7ea4c3686f9efdc850e3
diego40g <diegopaznaula@hotmail.com>
2023-08-16 00:26:02 -0500

    get all order by one user

--- src/routers/orders.ts
+++ src/routers/orders.ts
@@ -150,4 +150,17 @@ ordersRouter.get(`/get/count`,async (req,res) => {
     })
 })
 
+ordersRouter.get(`/get/userorders/:userid`,async (req ,res) => {
+    const userOrderList = await Order.find({user: req.params.userid}).populate({
+        path: 'orderItems', populate: {
+            path: 'product', populate: 'category'
+        }
+    }).sort({'dateOrdered':-1});
+
+    if(!userOrderList){
+        res.status(500).json({success: false})
+    }
+    res.status(201).send(userOrderList)
+})
+
 export default ordersRouter;
